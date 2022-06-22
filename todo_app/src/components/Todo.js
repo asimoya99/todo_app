749a92e1f959982578778489dee37c76bf7d81e1
Alessio Simoya <asimoya@u.rochester.edu>
2022-06-21 23:30:31 -0400

    first step in the to do app

---
+++ todo_app/src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h2>TITLE</h2>
+        <div className='actions'>
+          <button className='btn'> DELETE</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Todo
