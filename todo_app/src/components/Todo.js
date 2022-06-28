463d8cbda5969624cb8122cd0af57aa42a5be4b1
Alessio Simoya <asimoya@u.rochester.edu>
2022-06-28 17:13:59 -0400

    Modal working

--- todo_app/src/components/Todo.js
+++ todo_app/src/components/Todo.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Backdrop from './Backdrop'
 
+import Modal from './Modal'
 const Todo = (props) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const deleteHandler = () => {
+    setModalIsOpen(true)
+  }
+
+  const onCloseHandler = () => {
+    setModalIsOpen(false)
+  }
+  const closeModalHandler = () => {
+    setModalIsOpen(false)
+  }
   return (
-    <div>
-      <div className='card'>
-        <h2>{props.text}</h2>
-        <div className='actions'>
-          <button className='btn'> DELETE</button>
-        </div>
+    <div className='card'>
+      <h2>{props.text}</h2>
+      <div className='actions'>
+        <button className='btn' onClick={deleteHandler}>
+          {' '}
+          DELETE
+        </button>
       </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onClick={onCloseHandler} />}
     </div>
   )
 }
